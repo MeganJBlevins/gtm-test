fe084ee19eb2de85b9eb410148baeaa90d3fcd33
Megan Blevins <mblevins@marlinnetwork.com>
2021-02-18 13:59:50 -0600

    updating gtm

--- plugins/gtm.js
+++ plugins/gtm.js
@@ -11,7 +11,7 @@ export default ({ app }) => {
     }); var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'G-96ZL1EVKG2');
+  })(window, document, 'script', 'dataLayer', 'GTM-NW5X9BC');
 }
 
 /* Since we're using virtual pageviews, disable your GTM normal pageview tag, it will fire on first load causing a duplicate with the virtual one fired from the router */
